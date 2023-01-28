30f8090f694842ba5689740d08a6978041ad703f
vargasdm <112888008+vargasdm@users.noreply.github.com>
2023-01-28 11:51:39 -0500

    added route for signing up

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,26 +1,44 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-
-// CREATE new user
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    });
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+          });
 
     req.session.save(() => {
-      req.session.loggedIn = true;
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
 
       res.status(200).json(userData);
     });
   } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
+    res.status(400).json(err);
   }
 });
+
+// CREATE new user
+// router.post('/', async (req, res) => {
+//   try {
+//     const userData = await User.create({
+//       username: req.body.username,
+//       email: req.body.email,
+//       password: req.body.password,
+//     });
+
+//     req.session.save(() => {
+//       req.session.loggedIn = true;
+
+//       res.status(200).json(userData);
+//     });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// });
 
 router.post('/login', async (req, res) => {
   try {
