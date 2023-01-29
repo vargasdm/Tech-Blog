ae73a6dfef78542bdd6235b66f67aa0fb87160e3
vargasdm <112888008+vargasdm@users.noreply.github.com>
2023-01-28 19:53:13 -0500

    changed db name

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -24,9 +24,25 @@ router.post('/', withAuth, async (req, res) => {
         user_id: req.session.user_id,
       });
   
-      res.json(newComment);
+      res.status(200).json(newComment);
     } catch (err) {
       res.status(400).json(err);
+    }
+  });
+
+  router.get('/', async (req, res) => {
+    try {
+      const newComment = await Comment.findAll({
+        // include: [
+        //   {
+        //     model: User,
+        //     attributes: ['username'],
+        //   },
+        // ],
+      });
+      res.status(200).json(newComment);
+    } catch (err) {
+      res.status(500).json(err);
     }
   });
 
--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,3 @@
-DROP DATABASE IF EXISTS blog_db;
+DROP DATABASE IF EXISTS blog_db_tech;
 
-CREATE DATABASE blog_db;
+CREATE DATABASE blog_db_tech;
